74efc3003a4a5ffd73120335216bb9139f1edcd1
rems-38 <rem.mazzone@orange.fr>
2023-03-21 18:50:11 +0100

    Z2Z in add

--- Math_preliminaries.hs
+++ Math_preliminaries.hs
@@ -8,32 +8,39 @@ import ZsurNZ
 -----------------------------------------------------------------
 -- Définition de Z sur 2Z, instanciation dans la classe groupe --
 -----------------------------------------------------------------
--- newtype Z_sur_2Z = Z2Z Integer deriving (Show)
+newtype Z_sur_2Z = Z2Z Integer deriving (Show, Eq)
 
--- addMod2 :: Z_sur_2Z-> Z_sur_2Z -> Z_sur_2Z
--- addMod2 (Z2Z a) (Z2Z b) = Z2Z $ addModP 2 a b
+addMod2 :: Z_sur_2Z-> Z_sur_2Z -> Z_sur_2Z
+addMod2 (Z2Z a) (Z2Z b) = Z2Z $ addModP 2 a b
 
--- oppose2 :: Z_sur_2Z -> Z_sur_2Z
--- oppose2 (Z2Z n) = Z2Z $ oppose 2 n
+oppose2 :: Z_sur_2Z -> Z_sur_2Z
+oppose2 (Z2Z n) = Z2Z $ oppose 2 n
 
--- instance Group Z_sur_2Z where
---   unit = Z2Z 0
---   inverse = oppose2
---   operation = addMod2
+toZ2Z :: Z_sur_2Z -> Z_sur_2Z
+toZ2Z (Z2Z n) = Z2Z $ n `mod` 2
+
+instance Group Z_sur_2Z where
+  unit = Z2Z 0
+  inverse = oppose2
+  operation = addMod2
 -----------------------------------------------------------------
 
 -- newtype Polynome = Poly [Z_sur_2Z] deriving (Show)
 
-xor :: Integer -> Integer -> Integer
-xor a b | a == b = 0
-        | a /= b = 1
+-----------------------------------------------------------------
+-------------------------- Addition -----------------------------
+-----------------------------------------------------------------
+xor :: Z_sur_2Z -> Z_sur_2Z -> Z_sur_2Z
+xor a b | a == b = (Z2Z 0)
+        | a /= b = (Z2Z 1)
 
-addition :: [Integer] -> [Integer] -> [Integer]
+addition :: [Z_sur_2Z] -> [Z_sur_2Z] -> [Z_sur_2Z]
 addition [] [] = []
 addition nr [] = nr
 addition [] mr = mr
-addition (n:nr) (m:mr) = (xor n m) : (addition nr mr)
+addition (n:nr) (m:mr) = (xor (toZ2Z n) (toZ2Z m)) : (addition nr mr)
 
 -- addition :: Polynome -> Polynome -> Polynome
 -- addition (Poly []) (Poly []) = (Poly [])
 -- addition (Poly (n:nr)) (Poly (m:mr)) = Poly $ (xor n m) : (addition (Poly nr) (Poly mr))
+-----------------------------------------------------------------
