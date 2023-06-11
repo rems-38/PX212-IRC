836ceea5a5a5bd5815cf2b5fd12148b7d274a2f5
rems-38 <rem.mazzone@orange.fr>
2023-06-11 11:15:03 +0200

    Add + Multi C

--- c/main.c
+++ c/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	return 0;
+typedef unsigned char gf256;
+
+const gf256 p1 = 0x57, p2 = 0x83, resAdd = 0xd4, resMulti = 0xc1;
+
+gf256 add (gf256 a, gf256 b) { return a ^ b; }
+
+gf256 multi (gf256 a, gf256 b) {
+	gf256 res = 0, carry;
+
+	for (int i = 0; i < 8; i++) {
+		if (b & 1) res ^= a;
+		carry = a & 0x80;
+		a <<= 1;
+		if (carry) a ^= 0x1b;
+		b >>= 1;
+	}
+
+	return res;
+}
+
+
+int main (void){
+	printf("%i", resAdd == add(p1, p2));
+	printf("%i", resMulti == multi(p1, p2));
+
+	return 1;
 }
