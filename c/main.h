80c2352b87944dfad2749207fb88530ef7e0ee3e
rems-38 <rem.mazzone@orange.fr>
2023-06-12 15:49:55 +0200

    cipher 128 working !

--- c/main.h
+++ c/main.h
@@ -13,5 +13,5 @@ void rcon(int i, byte out[4]);
 void splitArr(const byte in[], byte out[], int start, int end);
 void mergeArr(const byte in[], byte out[], int start, int end);
 void keyExpansion(const byte key[], byte w[], int nk, int nr);
-void cipher(byte in[], byte out[], byte w[]);
+void cipher(byte in[], byte out[], byte w[], int nr);
 void printByte(byte in[], int length);
