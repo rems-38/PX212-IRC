cbde8b0dd5ba4e662cee9148f7b1b77d608beb7a
rems-38 <rem.mazzone@orange.fr>
2023-06-12 15:30:20 +0200

    keyExp OK (test 128, 192, 256)

--- c/main.h
+++ c/main.h
@@ -1,5 +1,6 @@
 typedef unsigned char byte;
 
+void byteXor(byte a[], const byte b[], int length);
 byte multi(byte a, byte b);
 void addRoundKey(byte state[], byte w[], int round);
 void subBytes(byte state[]);
@@ -9,6 +10,8 @@ void mixColumns(byte state[]);
 void subWord(byte state[4]);
 void rotWord(byte state[4]);
 void rcon(int i, byte out[4]);
-void keyExpansion(byte key[], byte w[]);
+void splitArr(const byte in[], byte out[], int start, int end);
+void mergeArr(const byte in[], byte out[], int start, int end);
+void keyExpansion(const byte key[], byte w[], int nk, int nr);
 void cipher(byte in[], byte out[], byte w[]);
 void printByte(byte in[], int length);
