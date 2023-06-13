e41dfafbac528be7b3dc6a1ab570b396addaed49
rems-38 <rem.mazzone@orange.fr>
2023-06-13 11:36:47 +0200

    encrypt + decrypt v1 (not working yet)

--- c/cipher.h
+++ c/cipher.h
@@ -8,6 +8,6 @@ void mixColumns(byte state[], const byte polyMix[16]);
 void subWord(byte state[4]);
 void rotWord(byte state[4]);
 void rcon(int i, byte out[4]);
-void keyExpansion(const byte key[], byte w[], int nk, int nr);
+void keyExpansion(byte key[], byte w[], int nk, int nr);
 void cipher(byte in[], byte w[], int nr);
 void invCipher(byte in[], byte w[], int nr);
