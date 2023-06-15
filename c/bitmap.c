6020b3620ce23706e2705d9c1a80c1a93c122a96
Guillemot Moussu <guillemot.moussu@gmail.com>
2023-06-15 10:22:34 +0200

    bitmap dechiffer ok

--- c/bitmap.c
+++ c/bitmap.c
@@ -1,3 +1,13 @@
+/** @file bitmap.c
+ *  @brief BMP encryption and decryption
+ *
+ *  Contient les fonctions de chiffrement et de déchiffrement pour des fichiers BMP.
+ *
+ *  @author Mazzone Rémi (rems-38)
+ *  @author Moussu Guillemot (guillemotmoussu)
+ *  @bug Fichiers illisibles sous Windows uniquement.
+ */
+
 #include "cipher.h"
 #include "tools.h"
 #include "aes.h"
@@ -18,7 +28,36 @@ void ecrireBMP(char* filename, unsigned char* info, unsigned char* data, int siz
     fclose(f);
 }
 
-void crypterBMP(char* filename) {
+void chiffrerBMP(char* filename) {
+    FILE *f = fopen(filename, "rb");
+    if (f == NULL) {
+        printf("Error opening file\n");
+        return;
+    }
+
+    unsigned char info[54];
+    fread(info, sizeof(unsigned char), 54, f);
+
+    int size = *(int*)&info[34];
+
+    unsigned char* data = malloc(size);
+    if (data == NULL) {
+        printf("Error allocating memory\n");
+        return;
+    }
+    
+    fread(data, sizeof(unsigned char), size, f);
+    
+    aes_encrypt(data, size, "0123456789abcdef", 16);
+
+    ecrireBMP("bitmap_files/bitmap_file_encrypted.bmp", info, data, size);
+    printf("Fichier chiffré en bitmap_files/bitmap_file_encrypted.bmp\n");
+   
+    fclose(f);
+    free(data);
+}
+
+void dechiffrerBMP(char* filename) {
     FILE *f = fopen(filename, "rb");
     if (f == NULL) {
         printf("Error opening file\n");
@@ -34,9 +73,10 @@ void crypterBMP(char* filename) {
     
     fread(data, sizeof(unsigned char), size, f);
     
-    aes_encrypt(data, size, "0123456789abcdef", 16);
+    aes_decrypt(data, size, "0123456789abcdef", 16);
 
-    ecrireBMP("bitmap_files/bitmap_file_encrypted.bmp", info, data, size);
+    ecrireBMP("bitmap_files/bitmap_file_decrypted.bmp", info, data, size);
+    printf("Fichier déchiffré en bitmap_files/bitmap_file_decrypted.bmp\n");
    
     fclose(f);
     free(data);
@@ -44,6 +84,7 @@ void crypterBMP(char* filename) {
 
 int main(void)
 {
-    crypterBMP("bitmap_files/bitmap_original.bmp");
+    chiffrerBMP("bitmap_files/bitmap_original.bmp");
+    dechiffrerBMP("bitmap_files/bitmap_file_encrypted.bmp");
     return 0;
 }
